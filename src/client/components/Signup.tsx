76c412ea096083e16f040d75693ac4873d25b5bd
bkandh30 <kandhari.bhavya20@gmail.com>
2025-11-24 15:20:03 -0700

    Added card components

--- src/client/components/Signup.tsx
+++ src/client/components/Signup.tsx
@@ -5,12 +5,14 @@ export const Signup = () => {
     <div className="join">
       <div>
         <label className="input validator join-item">
-          <Mail className="text-neutral-content" />
+          <Mail className="text-base-content" />
           <input type="email" placeholder="mail@site.com" required />
         </label>
         <div className="validator-hint hidden">Enter valid email address</div>
       </div>
-      <button className="btn btn-neutral join-item">Join</button>
+      <button className="btn btn-neutral join-item text-neutral-content">
+        Join
+      </button>
     </div>
   );
 };
