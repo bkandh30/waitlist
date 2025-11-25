e5de54fa14523ba4199670ac7d18ce0bc5b3469d
bkandh30 <kandhari.bhavya20@gmail.com>
2025-11-24 18:47:40 -0700

    Fixed linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,9 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginAstro from "eslint-plugin-astro";
 export default [
   ...eslintPluginAstro.configs.recommended,
   {
     files: ["*.astro", "*.ts", "*.tsx"],
     processor: "astro/client-side-ts", // <- Uses the "client-side-ts" processor.
-    rules: {
-    },
-  }
+    rules: {},
+  },
 ];
